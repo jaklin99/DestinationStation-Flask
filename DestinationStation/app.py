a73dad7a12495ba292a32c615d1cb07ff359eb26
Mihail Bondarenco <bondarenco.mihail@gmail.com>
2021-06-07 12:49:52 +0200

    feat(app) add main endpoint for predictions

--- DestinationStation/app.py
+++ DestinationStation/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask import request
+from datetime import datetime
 
 app = Flask(__name__)
 
 
-@app.route('/')
+# /delays?departureStation=ASD&destinationStation=UT&rideTime=2021-06-31T13:59
+@app.route('/delays',  methods=['GET'])
 def hello_world():
+    departure = request.args.get('departureStation', 'UT')
+    destination = request.args.get('destinationStation', 'ASD')
+    rideTime = request.args.get(
+        'rideTime',
+        datetime.now().strftime("%Y-%m-%dT%H:%M")
+    )
+    print(departure, destination, rideTime)
+
     return 'Hello World!'
 
 
