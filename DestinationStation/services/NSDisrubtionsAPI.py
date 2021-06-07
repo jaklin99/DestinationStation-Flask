2ec330c2aa76ebbb8a8d187064f0711cba4b5ddc
Rostislav Ivanov <i425588@fhict.nl>
2021-06-07 14:45:11 +0200

    initial api call

---
+++ DestinationStation/services/NSDisrubtionsAPI.py
@@ -0,0 +1,17 @@
+import requests
+
+
+class NSDisrubtionsAPI:
+
+    def __init__(self, key) -> None:
+        self.key = 'de123543b4934bbdaea411ccb85e6a41'
+
+    def getDisrubtion(stationCode):
+        url = f'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/disruptions/station/{stationCode}'
+        print(url)
+
+        request = requests.get(
+            url, headers={"Ocp-Apim-Subscription-Key": 'de123543b4934bbdaea411ccb85e6a41'}
+        )
+
+        return str(request.json())
