978c6420018fd014e4c2010e8bfaeb99021f3093
Rostislav Ivanov <i425588@fhict.nl>
2021-06-08 12:29:37 +0200

    maintenance

--- DestinationStation/services/NSDisrubtionsAPI.py
+++ DestinationStation/services/NSDisrubtionsAPI.py
@@ -1,17 +1,32 @@
 import requests
-
+import datetime
+#from flask_restful import Resource
 
 class NSDisrubtionsAPI:
 
-    def __init__(self, key) -> None:
+    def __init__(self) -> None:
         self.key = 'de123543b4934bbdaea411ccb85e6a41'
 
-    def getDisrubtion(stationCode):
-        url = f'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/disruptions/station/{stationCode}'
+    def getDisrubtions(self, stationCode, time):
+        url = f'https://gateway.apiportal.ns.nl/reisinformatie-api/api/v3/disruptions?type=MAINTENANCE'
         print(url)
 
         request = requests.get(
-            url, headers={"Ocp-Apim-Subscription-Key": 'de123543b4934bbdaea411ccb85e6a41'}
+            url, headers={"Ocp-Apim-Subscription-Key": self.key}
         )
+        data = request.json()
+        d=0    #used to iterate over the data
+        for record in data:
+            for delay in data[d]['publicationSections'][0]['section']['stations']:   #iterates over every station mentioned in the data
+             if delay['stationCode'] == stationCode:
+                 if data[d]['end'] > time > data[d]['start']:
+                     return '1'
+            d = d+1
+        return '0'
+        # if data:
+        #     if data['type'] == "MAINTENANCE":
+        #         if datetime.data['end'] > datetime.time > datetime.data['start']:
+        #             return True
+        # else:
+        #     return 'kur'
 
-        return request.json()
